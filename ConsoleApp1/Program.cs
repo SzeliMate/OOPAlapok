5a8d06bd43ca59a9de29c3e58e70cdfbdab50a16
Szeli <szelim@kkszki.hu>
2025-09-17 12:21:38 +0200

    Feladat is ready.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class Szemely
+    {
+        public string _nev = "Nagy Ilona";
+        public int _kor = 35;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine(tanulo1._nev);
+            Console.WriteLine(tanulo1._kor);
         }
     }
 }
